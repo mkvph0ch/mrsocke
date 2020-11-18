bdde6f20ee0e57a89338a9138d5336415f6444ee
mkvph0ch <62255328+mkvph0ch@users.noreply.github.com>
2020-11-18 20:40:58 +0100

    Background Hickelkasten

--- Hickelkasten.rb
+++ Hickelkasten.rb
@@ -1,11 +1,29 @@
 define :ambient_loop do
-  live_loop :arp2 do
-    use_synth :fm
-    with_fx :reverb, mix: 1, room: 1 do
-      play (scale :f3, :minor_pentatonic, num_octaves: 1).tick, amp: 0.15, release: 0.5
-      sleep 0.25
+  
+  in_thread do
+    live_loop :slick_tick do
+      cue :stick
+      use_synth :fm
+      with_fx :reverb, mix: 1, room: 1 do
+        play (scale :f3, :minor_pentatonic, num_octaves: 1).tick, amp: 0.3, release: 0.5
+        sleep 0.25
+      end
     end
   end
+  
+  in_thread do
+    live_loop :bg do
+      sync :stick
+      use_synth :fm
+      with_fx :reverb, mix: 1, room: 1 do
+        play [(chord :Ab, :major7), (chord :Db, :major7), (chord :Bb, :minor7), (chord :Eb, '7')].ring.tick, attack: 0.1, release: 1, amp: 0.1
+        sleep 1
+      end
+    end
+  end
+  
 end
 
 ambient_loop
+
+
