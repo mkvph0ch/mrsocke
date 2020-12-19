2f5cb8bb026bef3c03f03819198fdd4c182cb76d
mkvph0ch <62255328+mkvph0ch@users.noreply.github.com>
2020-12-19 17:30:10 +0100

    ThommyBoy PATH

--- ThommyBoy/TB.rb
+++ ThommyBoy/TB.rb
@@ -1,3 +1,14 @@
+#Windows
+#TB_01 = "C:\Samples\ThommyBoy\TB_01.flac"
+#TB_02 = "C:\Samples\ThommyBoy\TB_02.flac"
+#TB_03 = "C:\Samples\ThommyBoy\TB_03.flac"
+#TB_04 = "C:\Samples\ThommyBoy\TB_04.flac"
+#Unix
+TB_01 = "/Samples/ThommyBoy/TB_01.flac"
+TB_02 = "/Samples/ThommyBoy/TB_02.flac"
+TB_03 = "/Samples/ThommyBoy/TB_03.flac"
+TB_04 = "/Samples/ThommyBoy/TB_04.flac"
+
 i = 1
 3.times do
   with_fx :gverb do
@@ -6,11 +17,11 @@ i = 1
     play choose(chord(:c1, :minor)), release: 8, cutoff: rrand(70, 130)
     sleep 4
     if i == 1
-      sample "/Users/mkv/Documents/dev/SonicPi/mrsocke/TB_01.flac", amp: 2
+      sample TB_01, amp: 2
     elsif i == 2
-      sample "/Users/mkv/Documents/dev/SonicPi/mrsocke/TB_02.flac", amp: 2
+      sample TB_02, amp: 2
     elsif i == 3
-      sample "/Users/mkv/Documents/dev/SonicPi/mrsocke/TB_03.flac", amp: 2
+      sample TB_03, amp: 2
     end
     sleep 4
     i += 1
@@ -41,13 +52,13 @@ in_thread do
     live_loop :happyb do
       sleep 10
       if j == 1
-        sample "/Users/mkv/Documents/dev/SonicPi/mrsocke/TB_04.flac", amp: 2
+        sample TB_04, amp: 2
       elsif j == 2
-        sample "/Users/mkv/Documents/dev/SonicPi/mrsocke/TB_04.flac", amp: 2, pitch: 8
+        sample TB_04, amp: 2, pitch: 8
       elsif j == 3
-        sample "/Users/mkv/Documents/dev/SonicPi/mrsocke/TB_04.flac", amp: 4, pitch: -8
+        sample TB_04, amp: 4, pitch: -8
       else
-        sample "/Users/mkv/Documents/dev/SonicPi/mrsocke/TB_04.flac"
+        sample TB_04
       end
       j += 1
     end
