ed71013cbec75ff96f0fe0559210d8f7a159339c
mkvph0ch <62255328+mkvph0ch@users.noreply.github.com>
2021-02-01 20:28:15 +0100

    brunftzeit drums

---
+++ Brunftzeit/brunftzeit_drums.rb
@@ -0,0 +1,47 @@
+i=1
+n=10
+
+live_loop :drums do
+  
+  if i%2 == 0
+    
+    1.times do
+      sample :drum_bass_hard
+      sleep 0.25
+      sample :drum_bass_hard
+      sleep 0.3
+      sample :drum_bass_hard
+      sleep 0.5
+      sample :drum_bass_hard
+      sleep 2
+    end
+    
+  else
+    in_thread do
+      
+      n.times do
+        sample :drum_heavy_kick
+        sleep 0.75
+      end
+      
+    end
+    
+    in_thread do
+      
+      n.times do
+        sample :drum_snare_hard, amp: 0.3, rate: rrand(-0.7, -1)
+        sleep 1.5
+      end
+    end
+    
+    n.times do
+      sleep 0.5
+      sample :drum_bass_hard
+      sleep 0.5
+    end
+    
+  end
+  
+  i=i+1
+  
+end
