6bf2a8ea8f8a4af80ec38a2526779590ac474169
mkvph0ch <62255328+mkvph0ch@users.noreply.github.com>
2021-02-01 20:34:46 +0100

    brunftzeit update sync

--- Brunftzeit/brunftzeit.rb
+++ Brunftzeit/brunftzeit.rb
@@ -1,33 +1,74 @@
-live_loop :my do
+i=1
+n=10
+
+live_loop :drums do
   
-  rep = rrand(4,12)
+  if i%2 == 0
+    
+    1.times do
+      sample :drum_bass_hard
+      sleep 0.25
+      sample :drum_bass_hard
+      sleep 0.3
+      sample :drum_bass_hard
+      sleep 0.5
+      sample :drum_bass_hard
+      sleep 1
+    end
+    
+  else
+    in_thread do
+      
+      n.times do
+        sample :drum_heavy_kick
+        sleep 0.75
+      end
+      
+    end
+    
+    in_thread do
+      
+      n.times do
+        sample :drum_snare_hard, amp: 0.3, rate: rrand(-0.7, -1)
+        sleep 1.5
+      end
+      
+    end
+    
+    (n-2).times do
+      sleep 0.5
+      sample :drum_bass_hard
+      sleep 0.5
+    end
+    
+  end
+  
+  i=i+1
+  
+end
+
+live_loop :bass_bg do
+  
+  rep = rrand(4,8)
   sleep rep*(sample_duration :bass_drop_c)
   
-  rep.times do
-    sample :bass_drop_c, amp: 0.35, rate: -1
-    sleep 0.15
+  with_fx :panslicer, mix: 1, phase: 5, invert_wave: 1, wave: 3 do
+    
+    rep.times do
+      sample :bass_drop_c, amp: 0.35, rate: -1
+      sleep 0.15
+    end
   end
+  
   sleep 2.15
-  rep.times do
-    sample :bass_drop_c, amp: 0.35
-    sleep 0.15
+  
+  with_fx :panslicer, mix: 1, phase: 5, invert_wave: 0, wave: 3 do
+    
+    rep.times do
+      sample :bass_drop_c, amp: 0.35
+      sleep 0.15
+    end
   end
+  
 end
 
-live_loop :lopo1 do
-  sample :drum_heavy_kick
-  sleep 0.75
-end
-
-live_loop :lopo2 do
-  sample :drum_snare_hard, amp: 0.3, rate: rrand(-0.7, -1)
-  sleep 1.5
-  #sample :drum_snare_soft, amp: 0.3, rate: 0.8
-  #sleep 0.75
-end
-
-live_loop :lopo3 do
-  sleep 0.5
-  sample :drum_bass_hard
-  sleep 0.5
-end
